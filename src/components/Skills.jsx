b56c127cab34a5860faff98d883d92148740a05e
minsik2434 <minsik2434@gmail.com>
2024-07-18 19:18:20 +0900

    Feat : Skills 반응형

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,14 +22,14 @@ const Skills = forwardRef((props, ref) => {
   return (
     <div className="bg-[#f6f6fe]">
       <div className="pt-[78px] px-[50px]" ref={ref}>
-        <h2 className="pl-[30px] text-[40px] border-t-[2px] border-black pt-[10px] text-center font-bold">
+        <h2 className="pl-[30px] text-[40px] tablet:text-[30px] mobile:text-[25px] border-t-[2px] border-black pt-[10px] text-center font-bold">
           Skills
         </h2>
         <div>
-          <div className="py-[50px] px-[30px] w-full flex flex-col gap-[50px]">
+          <div className="py-[50px] w-full flex flex-col gap-[50px] mobile:gap-[30px]">
             <div className="px-[50px] py-[20px] bg-white rounded-[70px]">
               <h3 className="text-[25px] font-semibold">Back-End</h3>
-              <div className="flex items-center gap-12 mt-[30px] flex-wrap">
+              <div className="flex mobile:flex-col mobile:items-start items-center gap-12 mobile:gap-4 mt-[30px] mobile:mt-[20px] flex-wrap">
                 <Icon img={Java} name="JAVA" />
                 <Icon img={Spring} name="Spring" />
                 <Icon img={SpringBoot} name="Spring Boot" />
@@ -42,14 +42,14 @@ const Skills = forwardRef((props, ref) => {
             </div>
             <div className="px-[50px] py-[20px] bg-white rounded-[70px]">
               <h3 className="text-[25px] font-semibold">Cloud</h3>
-              <div className="flex items-center gap-12 mt-[30px] flex-wrap">
+              <div className="flex mobile:flex-col mobile:items-start items-center gap-12 mobile:gap-4 mt-[30px] mobile:mt-[20px] flex-wrap">
                 <Icon img={Ec2} name="EC2" />
                 <Icon img={S3} name="S3" />
               </div>
             </div>
             <div className="px-[50px] py-[20px] bg-white rounded-[70px]">
               <h3 className="text-[25px] font-semibold">Front-End</h3>
-              <div className="flex items-center gap-12 mt-[30px] flex-wrap">
+              <div className="flex mobile:flex-col mobile:items-start items-center gap-12 mobile:gap-4 mt-[30px] mobile:mt-[20px] flex-wrap">
                 <Icon img={Html} name="HTML" />
                 <Icon img={Css} name="CSS" />
                 <Icon img={Tailwind} name="Tailwind" />
@@ -59,7 +59,7 @@ const Skills = forwardRef((props, ref) => {
             </div>
             <div className="px-[50px] py-[20px] bg-white rounded-[70px]">
               <h3 className="text-[25px] font-semibold">ETC</h3>
-              <div className="flex items-center gap-12 mt-[30px] flex-wrap">
+              <div className="flex mobile:flex-col mobile:items-start items-center gap-12 mobile:gap-4 mt-[30px] mobile:mt-[20px] flex-wrap">
                 <Icon img={Git} name="Git" />
                 <Icon img={Github} name="Github" />
               </div>
--- src/components/skills/Icon.jsx
+++ src/components/skills/Icon.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Icon({ img, name }) {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex desktop:flex-col tablet:flex-col mobile:gap-8 justify-center items-center">
       <div
-        className="rounded-[40px] w-[110px] shadow-lg shadow-neutral-400
+        className="rounded-[40px] w-[110px] tablet:w-[70px] mobile:w-[50px] shadow-lg shadow-neutral-400
      bg-[#f8f9fa] hover:-translate-y-3 hover:transition-all duration-200"
       >
         <img src={img} />
       </div>
-      <span className="text-center mt-[20px] text-[18px] font-semibold">
+      <span className="text-center mt-[20px] text-[18px] tablet:text-[14px] mobile:text-[10px] font-semibold">
         {name}
       </span>
     </div>
